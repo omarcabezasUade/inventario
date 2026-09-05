824cf832bc2e50b9033ea4c89b676de1f8750e6c
Manuel Vassallo <229650986+mvassallo-uade@users.noreply.github.com>
2026-09-05 20:09:34 -0300

    Permitir reintentar códigos inexistentes o cancelar la búsqueda

--- crud.py
+++ crud.py
@@ -13,6 +13,22 @@ def buscar_producto(productos, codigo):
     return posicion
 
 
+def solicitar_producto(productos):
+    posicion = -1
+    if len(productos) == 0:
+        print("No hay productos cargados.")
+    else:
+        codigo = validar_entero("Código (0 para cancelar): ", 0)
+        while codigo != 0 and posicion == -1:
+            posicion = buscar_producto(productos, codigo)
+            if posicion == -1:
+                print("El código", codigo, "no ha sido encontrado. Intente nuevamente.")
+                codigo = validar_entero("Código (0 para cancelar): ", 0)
+        if codigo == 0:
+            print("Operación cancelada.")
+    return posicion
+
+
 def mostrar_producto(producto):
     print("Código:", producto[0])
     print("Nombre:", producto[1])
@@ -40,21 +56,15 @@ def alta_producto(productos, categorias):
 
 def consultar_producto(productos):
     print("CONSULTA DE PRODUCTO")
-    codigo = validar_entero("Código: ", 1)
-    posicion = buscar_producto(productos, codigo)
-    if posicion == -1:
-        print("El código", codigo, "no ha sido encontrado.")
-    else:
+    posicion = solicitar_producto(productos)
+    if posicion != -1:
         mostrar_producto(productos[posicion])
 
 
 def modificar_producto(productos, categorias):
     print("MODIFICACIÓN DE PRODUCTO")
-    codigo = validar_entero("Código: ", 1)
-    posicion = buscar_producto(productos, codigo)
-    if posicion == -1:
-        print("El código", codigo, "no ha sido encontrado.")
-    else:
+    posicion = solicitar_producto(productos)
+    if posicion != -1:
         mostrar_producto(productos[posicion])
         opcion = 0
         while opcion != 5:
@@ -76,11 +86,8 @@ def modificar_producto(productos, categorias):
 
 def eliminar_producto(productos):
     print("ELIMINACIÓN DE PRODUCTO")
-    codigo = validar_entero("Código: ", 1)
-    posicion = buscar_producto(productos, codigo)
-    if posicion == -1:
-        print("El código", codigo, "no ha sido encontrado.")
-    else:
+    posicion = solicitar_producto(productos)
+    if posicion != -1:
         mostrar_producto(productos[posicion])
         confirmacion = input("¿Confirma la eliminación? S/N: ")
         while confirmacion != "S" and confirmacion != "s" and confirmacion != "N" and confirmacion != "n":
